9888d98c78b8e2b71c9a2ba4e8e3d937fcb62ecc
m4loo <128167781+m4loo@users.noreply.github.com>
2024-02-08 14:53:10 +0500

    done?

--- module-main/src/main/java/com/mjc/school/configuration/ProgramConfiguration.java
+++ module-main/src/main/java/com/mjc/school/configuration/ProgramConfiguration.java
@@ -1,15 +1,40 @@
 package com.mjc.school.configuration;
 
+import com.mjc.school.controller.implementation.AuthorController;
+import com.mjc.school.controller.implementation.NewsController;
+import com.mjc.school.menu.MenuHelper;
+import com.mjc.school.repository.datasource.DataSource;
+import com.mjc.school.repository.implementation.AuthorRepository;
+import com.mjc.school.repository.implementation.NewsRepository;
+import com.mjc.school.service.implementation.AuthorService;
+import com.mjc.school.service.implementation.NewsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan(basePackages = {"com.mjc.school.repository",
-        "com.mjc.school.controller",
-        "com.mjc.school.service",
-        "com.mjc.school"})
+@ComponentScan(basePackages = {"com.mjc.school"})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ProgramConfiguration {
 
+    @Bean
+    public DataSource dataSource() {
+        return new DataSource();
+    }
+
+    @Bean
+    public NewsController newsController() {
+        return new NewsController(new NewsService(new NewsRepository(dataSource())));
+    }
+
+    @Bean
+    public AuthorController authorController() {
+        return new AuthorController(new AuthorService(new AuthorRepository(dataSource())));
+    }
+
+    @Bean
+    public MenuHelper menuHelper() {
+        return new MenuHelper(newsController(), authorController());
+    }
 }
